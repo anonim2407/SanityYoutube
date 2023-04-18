e8244ec3b97d6b09d9df9863194fad25dbcf9dbc
kiril2001 <kiril.kk2002@gmail.com>
2023-04-18 15:42:37 +0200

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header className="flex justify-between p-5 container max-w-7xl ">
+      <div className="flex items-center ">
+        <div>
+          <Link href={"/"}>
+            <img
+              className=" w-40 object-contain cursor-pointer"
+              src={"https://links.papareact.com/yvf"}
+              alt="Logo"
+            />
+          </Link>
+        </div>
+        <div className="hidden md:inline-flex items-center space-x-5 ">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full ">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sing in</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
